41296c9fc4e36ecf023a28ae4fc54914fcfbd563
Marsamema <88942065+Marsamema@users.noreply.github.com>
2021-11-20 16:28:18 -0500

    Update variables.tf

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -17,10 +17,10 @@ variable "availability_zones" {
   default = ["us-east-2a", "us-east-2b"]
 }
 variable "ubuntuami" {
-  default = "ami-08353a25e80beea3e"
+  default = "ami-0279c3b3186e54acd"
 }
 variable "redhatami" {
-  default = "ami-008a8487adc2b32ec"
+  default = "ami-0b0af3577fe5e3532"
 }
 variable "key_name" {
   default = "testkey"
